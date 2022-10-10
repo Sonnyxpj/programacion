df9b81b0fee0712ad697514bcea09ab809ef9587
Benjamín Garrido <benjamin.garrido.j@gmail.com>
2022-10-10 17:11:02 -0400

    :D

--- vectores_puntos.py
+++ vectores_puntos.py
@@ -1,6 +1,5 @@
 import	os
 
-
 # Vectores de puntos 
 # Hecho por Benjamín Garrido
 # 05/01/2022
@@ -39,32 +38,82 @@ def dimension(lista): #Separa las coordenadas por dimensiones
             tipo5.append(lista[elemt])   
     return tipo2, tipo3, tipo4, tipo5
 
-def depurador_lista2d(tipo2):
-    for x in tipo2[0]: 
-        print(x)
+def depurador_lista2d(coordenadas):
+    eje_x = []
+    eje_y = []
+    for x in coordenadas[0]: 
+        eje_x.append(x[0])
+    for y in coordenadas[0]:
+        eje_y.append(y[1])
+    
+    print(eje_x)
+    print()
+    print(eje_y)
 
+def depurador_lista3d(coordenadas):
+    eje_x = []
+    eje_y = []
+    eje_z = []
+    for x in coordenadas[1]:
+        eje_x.append(x[0])
+    for y in coordenadas[1]:
+        eje_y.append(y[1])
+    for z in coordenadas[1]:
+        eje_z.append(z[2])
+    
+    print(eje_x,"\n",eje_y,"\n",eje_z)
+    
+def depurador_lista4d(coordenadas):
+    eje_x = []
+    eje_y = []
+    eje_z = []
+    eje_w = []
+    for x in coordenadas[2]:
+        eje_x.append(x[0])
+    for y in coordenadas[2]:
+        eje_y.append(y[1])
+    for z in coordenadas[2]:
+        eje_z.append(z[2])
+    for w in coordenadas[2]:
+        eje_w.append(w[3])
+    
+    print(eje_x,"\n",eje_y,"\n",eje_z,"\n",eje_w)
 
-
-#def separador_puntos(tipo2): #Almacena en partes las coordenadas x e y
-#    eje_x = []
-#    eje_y = []
-#    tam = len(tipo2[0])
-#    i = 1
-#    while i <= 5:
-#        
-#        eje_x.append(tipo2[0][0])
-#        i +=1
-#    print(tam)       
-#    return eje_x, eje_y
+def depurador_lista5d(coordenadas):
+    eje_x = []
+    eje_y = []
+    eje_z = []
+    eje_w = []
+    eje_t = []
+    for x in coordenadas[3]:
+        eje_x.append(x[0])
+    for y in coordenadas[3]:
+        eje_y.append(y[1])
+    for z in coordenadas[3]:
+        eje_z.append(x[2])
+    for w in coordenadas[3]:
+        eje_w.append(x[3])
+    for t in coordenadas[3]:
+        eje_t.append(t[4])
+    
+    print(eje_x)
+    print()
+    print(eje_y)
+    print()
+    print(eje_z)
+    print()
+    print(eje_w)
+    print()
+    print(eje_t)
+    
 
 if __name__ == "__main__":
     limpia_pantalla()
     datos = leer_archivo()
     lista = almacenar_datos(datos)
-    tipo2 = dimension(lista)
-    #separador_puntos(tipo2)
-    depurador_lista(tipo2)
-    #print(separador_puntos(tipo2)[0])
+    coordenadas = dimension(lista)
+    depurador_lista2d(coordenadas)
+
 
     
 
