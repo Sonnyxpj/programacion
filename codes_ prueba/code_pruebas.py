936bdc167925627d786a35c925a0c4830b1c80d9
Benjamín Garrido <benjamin.garrido.j@gmail.com>
2022-10-25 22:49:19 -0400

    :D

--- codes_ prueba/code_pruebas.py
+++ codes_ prueba/code_pruebas.py
@@ -1,22 +1,43 @@
-import random as rd
 import os
-
+import math
 def clear(): #limpia la consola 
     if os.name == 'posix':
         os.system('clear')
     else:
         os.system('cls')
 
-def prueba():
+def leibniz(n):
     i = 0
-    figuras = ["0", "1", "2", "3", "4", "5", "6", "7"]
-    while i <= 5:
-        resultado = rd.choices(figuras, [30, 20, 20, 10, 10, 5, 3, 2], k=3)
-        print(resultado)
+    pi4 = 0 
+    while i < n:
+        pi4 = pi4 + ((-1)**i)/((2 * i) + 1)
         i += 1
-    return resultado
+    
+    return pi4 * 4
+
+def wallis(n):
+    
+    i = 1
+    pi2 = 1
+    while i <= n:
+        pi2 =  pi2 * ((2*i)/((2*i)-1)) * ((2*i)/((2*i)+1))
+        i += 1
+    return pi2 * 2
+
+def euler(n):
+
+    pi2 = 0
+    i = 0
+    
+    while i < n:
+        pi2 = pi2 +((2**math.factorial(i))*math.factorial(i)**2)/(math.factorial(2*i+1))
+        i +=1
+    return pi2 *2 
+
 
 if __name__ == "__main__":
     clear()
-    prueba()
-    print(prueba())
+    print(leibniz(50))
+    print(wallis(50))
+    print(euler(50))
+    
